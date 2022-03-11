5c6c9b86790601540ea983be2de5a11667cd0b80
Egor Maleev <e9126515199@icloud.com>
2022-03-11 18:41:12 +0500

    осталось только номера

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
 # pprint(contacts_list)
+contacts_list.pop(0)
 otch = []
 otchestvo = re.findall(r"(\w+)(вич|вна)", str(contacts_list))
 for el in otchestvo:
@@ -12,7 +13,7 @@ for el in otchestvo:
   for word in el:
     sur += word
   otch.append(sur)
-
+# otch.append(' ')
 
 FIO = re.findall(r"(\['\w+)('|\s)(')?(,)?(\s)?(\s)?(\s)?(')?(\w+)", str(contacts_list))
 list2 = []
@@ -24,10 +25,6 @@ for el in FIO:
         if letter != '[' and letter != "'":
           str += letter
       list2.append(str)
-list2.pop(0)
-list2.pop(0)
-
-
 i = 0
 surname_list = []
 name_list = []
@@ -37,16 +34,37 @@ while i < len(list2):
   else:
     surname_list.append(list2[i])
   i += 1
+count = 0
+
+for person in contacts_list:
+  person[0] = surname_list[count]
+  person[1] = name_list[count]
+  if count != len(otch):
+    person[2] = otch[count]
+  # print(person)
+  count += 1
+  # if count == len(otch):
+  #   break
 list = []
-tuple_name = tuple(name_list)
-tuple_surname = tuple(surname_list)
-tuple_otch = tuple(otch)
-list.append(tuple_surname)
-list.append(tuple_name)
-list.append(tuple_otch)
-
-
+for person in contacts_list:
+  if person[0] in list:
+    for pers in contacts_list:
+      if pers[0] == person[0] and pers[1] == person[1]:
+        if pers[3] == '':
+          pers[3] = person[3]
+        if pers[4] == '':
+          pers[4] = person[4]
+        if pers[5] == '':
+          pers[5] = person[5]
+        if pers[6] == '':
+          pers[6] = person[6]
+          # print(pers)
+        # contacts_list.append(pers)
+        contacts_list.remove(person)
+    print(person[0])
+    # contacts_list.remove(pers)
+  list.append(person[0])
+pprint(contacts_list)
 with open("phonebook.csv", "w") as file:
   datawriter = csv.writer(file, delimiter=",")
-  print(list)
-  datawriter.writerows(list)
+  datawriter.writerows(contacts_list)
