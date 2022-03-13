326bcbadcd88884b6df40e752e966840992933e9
Egor Maleev <e9126515199@icloud.com>
2022-03-13 16:50:21 +0500

    Урааааааааааа

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import csv
 with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-# pprint(contacts_list)
 contacts_list.pop(0)
 otch = []
 otchestvo = re.findall(r"(\w+)(вич|вна)", str(contacts_list))
@@ -13,18 +12,17 @@ for el in otchestvo:
   for word in el:
     sur += word
   otch.append(sur)
-# otch.append(' ')
 
 FIO = re.findall(r"(\['\w+)('|\s)(')?(,)?(\s)?(\s)?(\s)?(')?(\w+)", str(contacts_list))
 list2 = []
 for el in FIO:
   for word in el:
-    str = ''
+    stroka = ''
     if word != '' and word != ' ' and word != "'" and word != ",":
       for letter in word:
         if letter != '[' and letter != "'":
-          str += letter
-      list2.append(str)
+          stroka += letter
+      list2.append(stroka)
 i = 0
 surname_list = []
 name_list = []
@@ -41,10 +39,7 @@ for person in contacts_list:
   person[1] = name_list[count]
   if count != len(otch):
     person[2] = otch[count]
-  # print(person)
   count += 1
-  # if count == len(otch):
-  #   break
 list = []
 for person in contacts_list:
   if person[0] in list:
@@ -58,13 +53,13 @@ for person in contacts_list:
           pers[5] = person[5]
         if pers[6] == '':
           pers[6] = person[6]
-          # print(pers)
-        # contacts_list.append(pers)
         contacts_list.remove(person)
-    print(person[0])
-    # contacts_list.remove(pers)
   list.append(person[0])
-pprint(contacts_list)
+for person in contacts_list:
+  phones = r"(8|\+7)( )?\(?(\d\d\d)\)?( )?(-)?(\d\d\d)?(-)?(\d\d)?(-)?(\d+)?( )?"
+  person[5] = re.sub(phones, r"+7(\3)\6-\8-\10", person[5])
+  person[5] = re.sub(r"\(?(доб. )(\d+)\)?", r" доб.\2", person[5])
+  
 with open("phonebook.csv", "w") as file:
   datawriter = csv.writer(file, delimiter=",")
   datawriter.writerows(contacts_list)
